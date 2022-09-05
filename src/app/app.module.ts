c9221e0fea2817f7e6015d7266cbb0ea18468cbc
VANESSA CAROLINA PEREZ MENDOZA <VANESSA.PEREZME@correoaiep.cl>
2022-09-05 13:37:21 -0300

    noveno commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,7 +16,10 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule, 
     HttpClientModule
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    InAppBrowser
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Article } from 'src/app/interfaces';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class ArticleComponent implements OnInit {
   @Input() article:Article;
   @Input() index:number;
 
-  constructor(private iab:InAppBrowser) { }
+  constructor(private iab:InAppBrowser,private actionSheetCtrl:ActionSheetController) { }
 
   ngOnInit() {}
 
@@ -21,5 +22,42 @@ export class ArticleComponent implements OnInit {
    // window.open(this.article.url,'_blank');
    const browser = this.iab.create(this.article.url);
    browser.show();
+  }
+
+  async openMenu(){
+
+    const actionSheet = await this.actionSheetCtrl.create(
+      {
+        header:'options',
+        buttons: [
+          {
+          text:'Share',
+          icon : 'share-outline',
+          handler : ()=> this.onOpenArticle()
+          },
+          {
+            text:'Favorites',
+            icon : 'heart-outline',
+            handler : ()=> this.onToggleFavorite()
+          },
+          {
+              text:'Cancel',
+              icon : 'close-outline',
+              role: 'cancel'
+          }
+        ]
+    });
+
+    await actionSheet.present();
+
+  }
+
+  onOpenArticle(){
+    console.log('share article');
+  }
+
+  onToggleFavorite(){
+    console.log('share article');
+
   }
 }
